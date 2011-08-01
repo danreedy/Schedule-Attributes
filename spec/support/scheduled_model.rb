60165a48f11822824e12cb724d9eecb1e362c7f5
Dan Reedy <danreedy@gmail.com>
2011-08-01 11:36:07 -0500

    add accessors for additional_dates and exception_dates

--- spec/support/scheduled_model.rb
+++ spec/support/scheduled_model.rb
@@ -2,5 +2,5 @@ require 'schedule_atts'
 
 class ScheduledModel
   include ScheduleAttributes
-  attr_accessor :schedule_yaml
+  attr_accessor :schedule_yaml, :additional_dates, :exception_dates
 end
